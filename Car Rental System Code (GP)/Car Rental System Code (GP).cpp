0879c42490bba85c46ff675e0c7a246afa463ecd
Syed UwU <imranwafi06@gmail.com>
2026-09-10 09:48:35 +0800

    hgfds

--- Car Rental System Code (GP)/Car Rental System Code (GP).cpp
+++ Car Rental System Code (GP)/Car Rental System Code (GP).cpp
@@ -2,7 +2,7 @@
 #include <cstring> // For strcpy, strcmp, etc.
 #include <cmath>
 using namespace std;
-
+//hello
 // Function prototypes
 void   activitiesAssign(int, char[], char[]);
 int    activitiesOption(char[]);
